a00d4666dc96cf750b75df2fa4054c6d972a6ccb
Jose Familia <familiajoserene@gmail.com>
2026-01-20 21:31:54 -0400

    feat: upgrade to Astro 5 and @astrojs/vercel 9 for Node 22 support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import markdoc from "@astrojs/markdoc";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 import keystatic from "@keystatic/astro";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://josefamilia.vercel.app",
-  output: "hybrid",
   adapter: vercel(),
   integrations: [
     tailwind({
